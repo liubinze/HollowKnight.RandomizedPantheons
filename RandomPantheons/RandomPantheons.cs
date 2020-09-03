8ebc3340a4390193657a5b9a690bf82725b80327
5FiftySix6 <ybham6@gmail.com>
2020-09-03 16:16:44 -0400

    Rename, rewrite

---
+++ RandomPantheons/RandomPantheons.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Modding;
+using JetBrains.Annotations;
+using Random = System.Random;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
+using Vasi;
+
+namespace RandomPantheons
+{
+    [UsedImplicitly]
+    public class RandomPantheons : Mod, ITogglableMod
+    {
+        private static readonly List<string> Blacklist = new List<string>
+        {
+            "GG_Unn",
+            "GG_Wyrm",
+            "GG_Engine",
+            "GG_Engine_Prime",
+            "GG_Engine_Root",
+        };
+
+        // Some scenes cause issues when first.
+        private static readonly List<string> InvalidFirst = new List<string>()
+        {
+            // Causes an infinite loop
+            "GG_Spa",
+            // Doesn't wake up
+            "GG_Gruz_Mother",
+            "GG_Gruz_Mother_V"
+        };
+
+        private readonly Random _rand = new Random();
+
+        public override string GetVersion() => VersionUtil.GetVersion<RandomPantheons>();
+
+        public override void Initialize()
+        {
+            /*
+             * We need to hook on the door, rather than the controller
+             * Because the door has the challengeFSM responsible for the transition to the first scene
+             * Which is otherwise ignored if we edit the sequence later.
+             */
+            On.BossSequenceDoor.Start += RandomizeSequence;
+        }
+
+        private void RandomizeSequence
+        (
+            On.BossSequenceDoor.orig_Start orig,
+            BossSequenceDoor self
+        )
+        {
+            BossSequence seq = self.bossSequence;
+
+            ref BossScene[] bossScenes = ref Mirror.GetFieldRef<BossSequence, BossScene[]>(seq, "bossScenes");
+
+            List<BossScene> scenes = bossScenes
+                                     .Where(x => !Blacklist.Contains(x.sceneName))
+                                     .OrderBy(i => _rand.Next())
+                                     .ToList();
+            
+            const string bench = "GG_Spa";
+
+            while (InvalidFirst.Contains(scenes[0].sceneName))
+            {
+                BossScene first = scenes[0];
+
+                scenes.RemoveAt(0);
+
+                scenes.Insert(_rand.Next(1, scenes.Count), first);
+            }
+
+            // Multiple benches in a row causes an infinite loop.
+            for (int i = 0; i < scenes.Count - 1; i++)
+            {
+                if
+                (
+                    scenes[i].sceneName != bench
+                    || scenes[i].sceneName != scenes[i + 1].sceneName
+                )
+                {
+                    continue;
+                }
+
+                scenes.RemoveAt(i);
+
+                // Move the cursor one back, because otherwise we'll skip over an element.
+                i--;
+            }
+
+            bossScenes = scenes.ToArray();
+
+            orig(self);
+        }
+
+        public void Unload()
+        {
+            On.BossSequenceDoor.Start -= RandomizeSequence;
+        }
+    }
+}
